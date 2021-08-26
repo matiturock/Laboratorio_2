cf3f84681e2cf9dbf0ff669a84d2f5784120d299
Matias <matiturock@gmail.com>
2021-08-26 12:42:55 -0300

    Combiando Desafio 6

---
+++ Practica/src/Combinatoria.java
@@ -0,0 +1,6 @@
+public class Combinatoria {
+    public static void main(String[] args) {
+        float combiando = (((6 / 5) - (2 / 3) * (7 / 2) + (2 / 30)) / (((1 / 3) / 5)));
+        System.out.println(combiando);
+    }
+}
